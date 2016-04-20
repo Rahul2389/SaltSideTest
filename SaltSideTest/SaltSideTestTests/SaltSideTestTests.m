616f2d5fc066b1dbf4655a9367a668c7a9bd8d08
Rahul Borawar <rahul.borawar@commonfloor.com>
2016-04-20 12:49:24 +0530

    Adding Project Files

---
+++ SaltSideTest/SaltSideTestTests/SaltSideTestTests.m
@@ -0,0 +1,39 @@
+//
+//  SaltSideTestTests.m
+//  SaltSideTestTests
+//
+//  Created by Rahul on 4/20/16.
+//  Copyright © 2016 SaltSide. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SaltSideTestTests : XCTestCase
+
+@end
+
+@implementation SaltSideTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
